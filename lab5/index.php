4c9001ec90e0404e1d8f4e1a4d0a7b3ed71cdcee
Dragos-Paul Pop <dragos_paul_pop@yahoo.com>
2022-11-10 11:27:37 +0200

    lab5 edit

---
+++ lab5/index.php
@@ -0,0 +1,11 @@
+<?php  
+  require_once('./readDb.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include('./head.php'); ?>
+<body>
+  <?php include('./addForm.php'); ?>
+  <?php include('./table.php'); ?>
+</body>
+</html>
