6bf782fe9f3a4e159085dd6bc99b12c4c2c485e9
Dragos-Paul Pop <froind@gmail.com>
2022-11-10 11:28:00 +0200

    lab5 edit

---
+++ lab5/editForm.php
@@ -0,0 +1,39 @@
+<?php
+   $id = isset($_GET['id']) ? $_GET['id'] : null;
+   $userId = isset($_GET['userId']) ? $_GET['userId'] : null;
+   
+   if ($id && $userId) {
+    require_once('./readDb.php');
+
+    $title = null;
+
+    foreach($toDos as $todo) {
+      if ($todo['userId'] == $userId && $todo['id'] == $id) {
+        $title = $todo['title'];
+      }
+    }
+
+    if ($title === null) {
+      header('Location: index.php');          
+    }
+
+   } else {
+    header('Location: index.php');    
+   }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include('./head.php'); ?>
+<body>
+<form action="editTodo.php" method="post">
+  <input type="hidden" value="<?= $id; ?>" name="id">
+  <input type="hidden" value="<?= $userId; ?>" name="userId">
+  <input type="text" value="<?= $title; ?>" name="title">
+  <button class="waves-effect waves-light green btn">
+    <i class="material-icons white-text">save</i>
+  </button>
+</form>
+</body>
+</html>
